9ba6b2f4a54f6c6115b00707ecae852f4e8a3818
Vinh <nguyenvinh1997@yahoo.de>
2020-05-30 03:23:22 +0700

    navigation bar with icons

--- App.js
+++ App.js
@@ -6,16 +6,37 @@ import CartView from './components/CartView'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Entypo';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Shop' component={ShopView} />
-        <Tab.Screen name='Cart' component={CartView} />
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: false
+        }}
+      >
+        <Tab.Screen
+          name='Shop'
+          component={ShopView}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Icon name='shop' color={focused ? '#33cc00' : '#808080'} size={30} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name='Cart'
+          component={CartView}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Icon name='shopping-cart' color={focused ? '#33cc00' : '#808080'} size={30} />
+            )
+          }}
+        />
       </Tab.Navigator>
-    </NavigationContainer>
+    </NavigationContainer >
   );
 }
